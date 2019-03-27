5c983c134555e17923280d019e85751dfb36e847
秋月夜 <baojun.cz@gmail.com>
2019-03-27 14:42:15 +0800

    chore: make import to be StatelessWidget

--- lib/pages/import_wallet.dart
+++ lib/pages/import_wallet.dart
@@ -2,23 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:ckbalance/resources/strings.dart';
 import 'package:fluintl/fluintl.dart';
 import 'package:ckbalance/pages/input_password.dart';
-import 'package:bip39/bip39.dart' as bip39;
-import '../views/button/my_raised_button.dart';
 import '../views/mnemonic_field.dart';
 
-class ImportWalletPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _State();
-}
-
-class _State extends State<ImportWalletPage> {
-  final GlobalKey<FormFieldState<String>> _fieldKey = GlobalKey<FormFieldState<String>>();
-
-  _import() {
-    final FormFieldState<String> _field = _fieldKey.currentState;
-    if (_field.validate()) {}
-  }
-
+class ImportWalletPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
