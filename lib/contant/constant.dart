e6df754bcd1f5c448fb364110735cee9b4d07256
Bao Jun <baojun.cz@gmail.com>
2019-05-18 11:07:15 +0800

    feat: add node url to my server

--- lib/contant/constant.dart
+++ lib/contant/constant.dart
@@ -1,7 +1,7 @@
 import 'package:ckb_sdk/ckb-utils/network.dart';
 
 final maxThinBlocksSize = 100;
-final nodeUrl = 'http://47.111.175.189:8121';
+final nodeUrl = 'http://www.slys.in:8114';
 final network = Network.TestNet;
 
 final CKBUnit = 'CKB';
